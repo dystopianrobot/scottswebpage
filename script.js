58c848ec56264bc52bbd6af5f485c49faf69dd71
Scott Ward <skwkcaeow@gmail.com>
2026-09-04 00:15:57 -0700

    Implement pop-up functionality in script.js
    Added a pop-up functionality that activates after 30 seconds and can be closed by clicking the close button or outside the content box.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-This will be my javascript file
+/* 
+window.addEventListener('load', () => {
+  const popupOverlay = document.getElementById('popup-overlay');
+  const closeButton = document.getElementById('close-popup');
+
+  // Show the pop-up after 30 seconds (30,000 milliseconds)
+  setTimeout(() => {
+    popupOverlay.classList.add('active');
+  }, 30000);
+
+  // Close pop-up when clicking the "X" button
+  closeButton.addEventListener('click', () => {
+    popupOverlay.classList.remove('active');
+  });
+
+  // Optional: Close pop-up when clicking anywhere outside the content box
+  popupOverlay.addEventListener('click', (event) => {
+    if (event.target === popupOverlay) {
+      popupOverlay.classList.remove('active');
+    }
+  });
+});
+*/
